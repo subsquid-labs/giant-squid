f2d7b6a9a3f9da63cf7aac3b0f9baa3d60a11759
Alexander Belopashentsev <abelopashentsev@gmail.com>
2022-01-18 16:00:15 +0500

    changed batch size

--- src/processor.ts
+++ src/processor.ts
@@ -7,55 +7,30 @@ const processor = new SubstrateProcessor('polkadot_balances')
 
 
 processor.setTypesBundle('polkadot')
-processor.setBatchSize(500)
-
+processor.setBatchSize(150)
 
 processor.setDataSource({
     archive: 'https://polkadot.indexer.gc.subsquid.io/v4/graphql',
     chain: 'wss://rpc.polkadot.io'
 })
+//{range: {from:6661791 }}
+processor.addEventHandler('balances.Endowed', balanceHandlers.handleEndowedEvent)
+processor.addEventHandler('balances.DustLost', balanceHandlers.handleDustLostEvent)
+processor.addEventHandler('balances.Transfer', balanceHandlers.handleTransferEvent)
+processor.addEventHandler('balances.BalanceSet', balanceHandlers.handleBalanceSetEvent)
+processor.addEventHandler('balances.Reserved', balanceHandlers.handleReservedEvent)
+processor.addEventHandler('balances.Unreserved', balanceHandlers.handleUnreservedEvent)
+processor.addEventHandler('balances.ReserveRepatriated', balanceHandlers.handleReserveRepatriatedEvent)
+processor.addEventHandler('balances.Deposit', balanceHandlers.handleDepositEvent)
+processor.addEventHandler('balances.Withdraw', balanceHandlers.handleWithdrawEvent)
+processor.addEventHandler('balances.Slashed', balanceHandlers.handleSlashedEvent)
 
-processor.addEventHandler('balances.Endowed',{range: {from:6661791 }}, balanceHandlers.handleEndowedEvent)
-processor.addEventHandler('balances.DustLost',{range: {from:6661791 }}, balanceHandlers.handleDustLostEvent)
-processor.addEventHandler('balances.Transfer',{range: {from:6661791 }}, balanceHandlers.handleTransferEvent)
-processor.addEventHandler('balances.BalanceSet',{range: {from:6661791 }}, balanceHandlers.handleBalanceSetEvent)
-processor.addEventHandler('balances.Reserved',{range: {from:6661791 }}, balanceHandlers.handleReservedEvent)
-processor.addEventHandler('balances.Unreserved',{range: {from:6661791 }}, balanceHandlers.handleUnreservedEvent)
-processor.addEventHandler('balances.ReserveRepatriated',{range: {from:6661791 }}, balanceHandlers.handleReserveRepatriatedEvent)
-processor.addEventHandler('balances.Deposit',{range: {from:6661791 }}, balanceHandlers.handleDepositEvent)
-processor.addEventHandler('balances.Withdraw',{range: {from:6661791 }}, balanceHandlers.handleWithdrawEvent)
-processor.addEventHandler('balances.Slashed',{range: {from:6661791 }}, balanceHandlers.handleSlashedEvent)
-
-processor.addEventHandler('staking.Rewarded',{range: {from:6661791 }}, stakingHandlers.handleRewardedEvent)
-processor.addEventHandler('staking.Reward',{range: {from:6661791 }}, (ctx) => stakingHandlers.handleRewardedEvent(ctx, true))
-processor.addEventHandler('staking.Slashed',{range: {from:6661791 }}, stakingHandlers.handleSlashedEvent)
-processor.addEventHandler('staking.Slash',{range: {from:6661791 }}, (ctx) => stakingHandlers.handleSlashedEvent(ctx, true))
-processor.addEventHandler('staking.Bonded',{range: {from:6661791 }}, stakingHandlers.handleBondedEvent)
-processor.addEventHandler('staking.Unbonded',{range: {from:6661791 }}, stakingHandlers.handleUnbondedEvent)
-processor.addEventHandler('staking.Withdrawn',{range: {from:6661791 }}, stakingHandlers.handleWithdrawnEvent)
+processor.addEventHandler('staking.Rewarded', stakingHandlers.handleRewardedEvent)
+processor.addEventHandler('staking.Reward', (ctx) => stakingHandlers.handleRewardedEvent(ctx, true))
+processor.addEventHandler('staking.Slashed', stakingHandlers.handleSlashedEvent)
+processor.addEventHandler('staking.Slash', (ctx) => stakingHandlers.handleSlashedEvent(ctx, true))
+processor.addEventHandler('staking.Bonded', stakingHandlers.handleBondedEvent)
+processor.addEventHandler('staking.Unbonded', stakingHandlers.handleUnbondedEvent)
+processor.addEventHandler('staking.Withdrawn', stakingHandlers.handleWithdrawnEvent)
 
 processor.run()
-
-
-async function getOrCreate<T extends { id: string }>(
-    store: Store,
-    entityConstructor: EntityConstructor<T>,
-    id: string
-): Promise<T> {
-
-    let e = await store.get<T>(entityConstructor, {
-        where: { id },
-    })
-
-    if (e == null) {
-        e = new entityConstructor()
-        e.id = id
-    }
-
-    return e
-}
-
-
-type EntityConstructor<T> = {
-    new(...args: any[]): T
-}
