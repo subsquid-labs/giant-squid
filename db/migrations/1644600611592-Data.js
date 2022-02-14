38209f90d54e7b0ba6a3e8c9031562462475b22c
Alexander Belopashentsev <abelopashentsev@gmail.com>
2022-02-14 12:55:35 +0500

    fix migrations

--- db/migrations/1644600611592-Data.js
+++ db/migrations/1644600611592-Data.js
@@ -3,11 +3,8 @@ module.exports = class Data1644600611592 {
 
   async up(db) {
     await db.query(`DROP INDEX "public"."IDX_b56c98c7f752bb9c88b7a5696a"`)
-    await db.query(`DROP INDEX "public"."IDX_f9c2878a2b78726bd4db9edee4"`)
     await db.query(`DROP INDEX "public"."IDX_06f5863364b6acc2e315ceba39"`)
-    await db.query(`DROP INDEX "public"."IDX_2a4e1dce9f72514cd28f554ee2"`)
     await db.query(`DROP INDEX "public"."IDX_00fd800c7551b555933b7d1a1d"`)
-    await db.query(`DROP INDEX "public"."IDX_212058fe00a4e4ad6f43383399"`)
     await db.query(`ALTER TABLE "contribution" DROP COLUMN "extrinsic_id"`)
     await db.query(`ALTER TABLE "contribution" DROP COLUMN "event_id"`)
     await db.query(`ALTER TABLE "transfer" DROP COLUMN "extrinsic_id"`)
