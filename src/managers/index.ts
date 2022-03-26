c7af1d7e98e294cef634b9688e058d850cf583bc
Alexander Belopashentsev <abelopashentsev@gmail.com>
2022-03-26 17:15:44 +0500

    add entity managers

---
+++ src/managers/index.ts
@@ -0,0 +1,5 @@
+import { crowdloanManager } from './CrowdloanManager'
+import { chainManager } from './ChainManager'
+import { accountManager } from './AccountManager'
+
+export { crowdloanManager, chainManager, accountManager }
