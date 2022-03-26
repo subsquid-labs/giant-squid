7124bd4b4e9b0fa654eef3292e39781ad64b1a83
Alexander Belopashentsev <abelopashentsev@gmail.com>
2022-03-26 19:05:49 +0500

    add lastUpdateBlock to account

--- src/model/generated/account.model.ts
+++ src/model/generated/account.model.ts
@@ -47,4 +47,7 @@ export class Account {
 
   @Column_("jsonb", {transformer: {to: obj => obj.toJSON(), from: obj => new StakingInfo(undefined, marshal.nonNull(obj))}, nullable: false})
   stakingInfo!: StakingInfo
+
+  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+  lastUpdateBlock!: bigint
 }
