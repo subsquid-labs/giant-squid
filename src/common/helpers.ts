475414aa13b158f05ea33e4d9285859741964444
Alexander Belopashentsev <61732514+belopash@users.noreply.github.com>
2022-05-10 17:56:06 +0500

    remove ss58 encoding

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -3,33 +3,6 @@ import { EventHandlerContext, ExtrinsicHandlerContext } from '@subsquid/substrat
 import { EXTRINSIC_SUCCESS } from './consts'
 import { AssertionError } from 'assert'
 
-export function encodeID(ID: Uint8Array, prefix: string | number): string | null {
-    let ret: string | null
-    try {
-        ret = ss58.codec(prefix).encode(ID)
-    } catch (e) {
-        if (e instanceof AssertionError) {
-            ret = '0x' + (ID as Buffer).toString('hex')
-        } else {
-            console.error(e)
-            ret = null
-        }
-    }
-
-    return ret
-}
-
-export function decodeID(ID: string, prefix: string | number): Uint8Array | null {
-    let ret: Uint8Array | null
-    try {
-        ret = ss58.codec(prefix).decode(ID)
-    } catch (e) {
-        ret = null
-    }
-
-    return ret
-}
-
 export function populateMeta<T extends ItemBase>(ctx: ExtrinsicHandlerContext | EventHandlerContext, entity: T): void {
     entity.extrinsicHash = ctx.extrinsic?.hash
     entity.blockNumber = BigInt(ctx.block.height).valueOf()
