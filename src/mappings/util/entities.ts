4bdb2c1640d7335d286a25d824982c9dd183391f
Alexander Belopashentsev <abelopashentsev@gmail.com>
2022-06-27 03:23:11 +0500

    fix null values in transfer.type

--- src/mappings/util/entities.ts
+++ src/mappings/util/entities.ts
@@ -175,7 +175,7 @@ export interface TransferData extends ActionData {
 }
 
 export async function saveTransfer(ctx: CommonHandlerContext, data: TransferData) {
-    const { fromId, toId, amount, success } = data
+    const { fromId, toId, amount, success, type } = data
 
     const from = await getOrCreateAccount(ctx, fromId)
     const to = toId ? await getOrCreateAccount(ctx, toId) : null
@@ -185,14 +185,17 @@ export async function saveTransfer(ctx: CommonHandlerContext, data: TransferData
         from: new TransferLocationAccount({
             id: fromId,
         }),
-        to: new TransferLocationAccount({
-            id: toId,
-        }),
+        to: toId
+            ? new TransferLocationAccount({
+                  id: toId,
+              })
+            : null,
         asset: new TransferAssetToken({
             symbol: 'GLMR',
             amount,
         }),
         success,
+        type,
     })
 
     await ctx.store.insert(transfer)
@@ -202,7 +205,7 @@ export async function saveTransfer(ctx: CommonHandlerContext, data: TransferData
             id: `${transfer.id}-from`,
             transfer,
             account: from,
-            direction: TransferDirection.FROM,
+            direction: TransferDirection.From,
         })
     )
 
@@ -212,7 +215,7 @@ export async function saveTransfer(ctx: CommonHandlerContext, data: TransferData
                 id: `${transfer.id}-to`,
                 transfer,
                 account: to,
-                direction: TransferDirection.TO,
+                direction: TransferDirection.To,
             })
         )
     }
